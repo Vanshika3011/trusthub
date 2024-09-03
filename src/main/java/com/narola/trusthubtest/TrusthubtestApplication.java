e8b9714e7b14b03884a2a3a6b90363e66f17de65
Vanshika3011 <vjl@narola.email>
2024-09-03 15:06:31 +0530

    initial commit

---
+++ src/main/java/com/narola/trusthubtest/TrusthubtestApplication.java
@@ -0,0 +1,13 @@
+package com.narola.trusthubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrusthubtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrusthubtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/narola/trusthubtest/TrusthubtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narola.trusthubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrusthubtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
